2b94fde6c1469192d4159696c28e4bf72397d927
Laurene Eppelle <laurene.eppelle@gmail.com>
2022-10-20 23:06:05 +0200

    Add validations to trainer & client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,13 +1,13 @@
 class Client < ApplicationRecord
   belongs_to :user, inverse_of:  :clients
   accepts_nested_attributes_for :user
-
   has_many :bookings, dependent: :destroy, inverse_of:  :client
   accepts_nested_attributes_for :bookings
-
   has_many :lessons, through: :bookings
+  # has_many :lesson_payments, through: :bookings
 
-  has_many :lesson_payments, through: :bookings
+  validates :first_name, :last_name, :bio, presence: true
+  validates :bio, :experience, length: { maximum: 50 }
 
   def name
     "#{first_name} #{last_name}"

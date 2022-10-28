506c868d61684f6552a6a039cf18e004d9db7a20
Laurene Eppelle <laurene.eppelle@gmail.com>
2022-10-28 12:57:02 +0200

    Create trainer profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,13 @@ module ApplicationHelper
 
   def create_client_and_assign_to_current_user
       if current_user != nil && current_user.client == nil
-          client = Client.create!(user: current_user, first_name: current_user.first_name, last_name: current_user.last_name)
+          @client = Client.create!(user: current_user, first_name: current_user.first_name, last_name: current_user.last_name)
       end
+  end
+
+  def create_trainer_and_assign_to_current_user
+    if current_user != nil && current_user.is_trainer?
+      @trainer = Trainer.create!(user: current_user, first_name: current_user.first_name, last_name: current_user.last_name)
+    end
   end
 end

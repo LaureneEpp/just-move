a59063dc4694f24ba8abcce09c8a7d5adcab6de2
LaureneEpp <laurene.eppelle@gmail.com>
2022-12-05 21:52:50 +0100

    Add like/unlike button to lesson page

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -6,6 +6,7 @@ class Lesson < ApplicationRecord
 
   has_many :trainers, through: :bookings
   has_many :clients, through: :bookings
+  has_many :likes
 
   validates :title, :duration, :category, :language, :level, :description, presence: true
   validates :description, length: { maximum: 50 }

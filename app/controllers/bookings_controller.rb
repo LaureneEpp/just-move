13b85f42328b1033e58479ceb55acf5439b02cf4
LaureneEpp <laurene.eppelle@gmail.com>
2023-01-12 20:04:25 +0100

    no booking message + new lesson button

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,12 @@
 class BookingsController < ApplicationController
 
   def index
-  #   if params[:all]
-  #     @bookings = Booking.all
-  #   else
-  #     @user = User.where("user_id = ?", current_user)
-  #     @bookings = current_user.bookings
-  #   end
+    #   if params[:all]
+    #     @bookings = Booking.all
+    #   else
+    #     @user = User.where("user_id = ?", current_user)
+    #     @bookings = current_user.bookings
+    #   end
     @bookings = Booking.where(client_id: current_user.client)
   end
 
@@ -15,20 +15,21 @@ class BookingsController < ApplicationController
     flash[:notice] = @booking.errors.full_messages.to_sentence unless @booking.save
     redirect_to @booking.schedule
   end
-  
+
   def destroy
     @booking = current_user.client.bookings.find(params[:id])
     @schedule = params[:schedule_id]
     @booking.destroy
     respond_to do |format|
-      format.html { redirect_to schedule, notice: "Your booking as been deleted." }
+      format.html { redirect_to schedule, notice: 'Your booking as been deleted.' }
       format.turbo_stream
     end
   end
 
   private
 
-    def booking_params
-      params.require(:booking).permit(:status, :title, :cost, :start, :cancellation_reason, :refunded, :schedule_id, :trainer_id, :lesson_id, :client_id)
-    end
+  def booking_params
+    params.require(:booking).permit(:status, :title, :cost, :start, :cancellation_reason, :refunded, :schedule_id,
+                                    :trainer_id, :lesson_id, :client_id)
+  end
 end

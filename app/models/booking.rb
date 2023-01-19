dc6e1e36970c61e836e32b1bd31e509fa2b2c316
LaureneEpp <laurene.eppelle@gmail.com>
2023-01-19 15:44:54 +0100

    set default value to pending

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
+
   # belongs_to :lesson, inverse_of:  :bookings, optional: true
   # accepts_nested_attributes_for :lesson
 
@@ -23,10 +24,10 @@ class Booking < ApplicationRecord
   belongs_to :client
   # accepts_nested_attributes_for :client
 
-  # enum :status [ :pending, :completed, :rejected, :no_show, :no_show_refunded, :canceled, :canceled_refunded ]
+  # enum :status [:pending, :completed, :rejected, :no_show, :no_show_refunded, :canceled, :canceled_refunded]
+  enum status: { pending: 0, completed: 1, rejected: 2, no_show: 3 }
 
   validates :schedule, presence: true
-  validates :client_id, uniqueness: {scope: :lesson_id}
+  validates :client_id, uniqueness: { scope: :lesson_id }
   # validates :status, :price, :start, presence: true
 end
-
